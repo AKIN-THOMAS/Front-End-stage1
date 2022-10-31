d8b829ad25f7bc340e64dc7b145b1b8b1f1b160f
AKIN-THOMAS <akinthomasbishop25@gmail.com>
2022-10-31 14:06:01 +0100

    Corrections App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import profile_pic from './photo/profile_pic.png';
 import hub_icon from './photo/hub_icon.png';
-import internship from './photo/internship.png';
+import internship from './photo/internship.svg';
 import I4G from './photo/I4G.png';
-import icon from './photo/Icon.png';
+import icon from './photo/icon.svg';
 import slack from './photo/slack.png';
+import hover from './photo/profile_hover.svg';
+import icon_hover from './photo/icon_hover.svg';
+import mobile_share from './photo/mobile_share.svg';
 import './App';
 
 import './App.css'
@@ -14,24 +17,44 @@ const App = () => {
         <body className="main">
             <section className="profile">
                 <div>
-                    <img className="profile_id"  src= {profile_pic} alt="profile picture"/>
-                    <p className="twitter">Annette Black</p>
-                    {/* <p className="slack">ABIMBOLA</p> // my slack name that was asked to be hidden */}
-                    {/* <div className="icon">
-                        <img src={icon} alt ="icon"/>
-                    </div> */}
+                    <div className="icon__1">
+                        <div className="icon">
+                            <img  classname="one" src={icon} alt ="icon"/>
+                            <img className="two" src={icon_hover}/>
+                            <img  className="mobile" src={mobile_share}/>
+                        </div>
+                        
+                    </div>
+                   <div className="profile">
+                        <img className="profile_id"  src= {profile_pic} alt="profile picture"/>
+                        <img className="profile_hover" src={hover}/>
+                   </div>
+                    <p className="twitter">Akin-Thomas Bishop</p>
+                    <p className="slack">ABIMBOLA</p>
                 </div>
             </section>
             <section>
                 <div className="row">
                     <div className="container">
                     <div className="linksection">
-                        <button className="twitter_zuri"> <a href="https://training.zuri.team/" target= 'blank'>Twitter</a></button>
-                        <button className="button_zuri"> <a href="https://training.zuri.team/" target= 'blank'>Zuri Team</a></button>
-                        <button className="zuri_books"> <a href="https://books.zuri.team/" target= 'blank'>Zuri Books</a></button>
-                        <button className="python_books"> <a href="https://books.zuri.team/" target= 'blank'>Python Books</a></button>
-                        <button className="check_coders"> <a href="https://background.zuri.team/" target= 'blank'>Background Check for Coders </a></button>
-                        <button className="design_books"> <a href="https://books.zuri.team/design-rules" target= 'blank'>Design Books </a></button>
+                        <div className="a-button">
+                            <a href="https://twitter.com/Bishopakint/" id="twitter" className="button" target= 'blank'>Twitter</a>
+                        </div>
+                        <div className="a-button">
+                            <a href="https://training.zuri.team/" id="btn__zuri" className="button" target= 'blank'>Zuri Team</a>
+                        </div>
+                        <div className="a-button">
+                            <a href="https://books.zuri.team/" id="books" className="button" target= 'blank'>Zuri Books</a>
+                        </div>
+                       <div className="a-button">
+                            <a href="https://books.zuri.team/" id="books__python" className="button" target= 'blank'>Python Books</a>
+                       </div>
+                       <div className="a-button">
+                            <a href="https://background.zuri.team/" id="pitch" className="button" target= 'blank'>Background Check for Coders </a>
+                       </div>
+                        <div className="a-button">
+                            <a href="https://books.zuri.team/design-rules" id="book__design" className="button" target= 'blank'>Design Books </a>
+                        </div>
                 </div>
                
                     </div>
@@ -39,7 +62,7 @@ const App = () => {
                
                     <div className="social-icons">
                         <img  src= {slack} alt="slack icon"/>
-                        <img  src= {hub_icon} alt =" github icon"/>
+                        <a href="https://github.com/AKIN-THOMAS"><img  src= {hub_icon} alt =" github icon"/></a>
                     </div>
             </section>
             <footer>
